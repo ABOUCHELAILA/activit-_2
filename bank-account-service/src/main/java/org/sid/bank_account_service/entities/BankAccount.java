b26fb5a57100921ec5acccd69450b089c3b3e3ed
ABOUCHELAILA <TON_EMAIL@exemple.com>
2025-10-23 13:26:12 +0100

    1. Créer un projet Spring Boot avec les dépendances Web, Spring Data JPA, H2, Lombok

--- bank-account-service/src/main/java/org/sid/bank_account_service/entities/BankAccount.java
+++ bank-account-service/src/main/java/org/sid/bank_account_service/entities/BankAccount.java
@@ -1,12 +1,26 @@
 package org.sid.bank_account_service.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
 import java.util.Date;
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class BankAccount {
 
-
-   private String id;
-   private Date createdAt;
+   @Id
+   private String accountId;
+   private double balance;
+   private LocalDate createAt;
+   private String currency;
 
 
 
